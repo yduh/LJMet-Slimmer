10e19aada8ae7200503d02713f946da1baaf354c
Yi-Ting Duh <kaliduh@gmail.com>
2019-07-16 10:17:34 -0500

    finish setup script; created the structure of samples; working on runCondorJob script

--- runCondorSlimmerJobs.py
+++ runCondorSlimmerJobs.py
@@ -1,5 +1,5 @@
 import os,shutil,datetime,time
-import getpass
+import glob
 from ROOT import *
 
 execfile("/uscms_data/d3/jmanagan/EOSSafeUtils.py")
@@ -10,9 +10,10 @@ shift = sys.argv[1]
 
 ##################################################################
 relbase = '/uscms_data/d1/yiting11/CMSSW_9_4_6_patch1/'
-inputDir = '%s/%s/' %(os.environ['ntupleWhere'],shift) #'/eos/uscms/store/user/lpcljm/2018/LJMet94X_1lep_013019/'+shift+'/'
-outputDir = '%s/%s/' %(os.environ['slimrootWhere'],shift) #'/eos/uscms/store/user/lpcljm/yiting/2017/Feb/'+shift+'/'
-condorDir = '%s/%s/' %(os.environ['logbase'],shift) #relbase+'/src/LJMet-Slimmer/log/2017/Feb/'+shift+'/'
+inputDir = '%s/' %(os.environ['ntupleWhere']) #'/eos/uscms/store/user/lpcljm/FWLJMET102X_1lep2017_052219/
+outputDir = '%s/%s/' %(os.environ['slimrootWhere'],shift) #'/eos/uscms/store/user/lpcljm/yiting/2017/FWLJMET/'+shift+'/'
+condorDir = '%s/%s/' %(os.environ['logbase'],shift) #relbase+'/src/LJMet-Slimmer/log/2017/FWLJMET/'+shift+'/'
+sampleCatTag = '/%s/' %os.environ['sampleCatTag']
 
 inDir=inputDir
 outDir=outputDir
@@ -24,7 +25,7 @@ print 'Files from',runDirPost
 ###################################################################
 with open('samples/signalList.txt') as f:
     lines = f.readlines()
-signalList = [line.rstrip() for line in lines if ('#' not in line and line != '\n')]
+signalList = [line.rstrip() for line in lines if ('#' not in line and line != '\n')] #if there is no # in the line & not a empty line, we read it
 
 with open('samples/ttbarList.txt') as f:
     lines = f.readlines()
@@ -45,10 +46,11 @@ print 'signalList = ', signalList
 print 'ttbarList = ', ttbarList
 print 'dirList = ', dirList
 
+#For those we need to further separate into several different samples
 signalOutList = ['BWBW','TZBW','THBW','TZTH','TZTZ','THTH']
 ttbarOutList = ['Mtt0to700','Mtt700to1000','Mtt1000toInf']
 
-os._exit(1)
+#os._exit(1)
 ##################################################################
 
 gROOT.ProcessLine('.x compileStep1.C')
@@ -109,52 +111,6 @@ Queue 1"""%dict)
             print count, "jobs submitted!!!"
             print "You can kill jobs by: condor_rm -all -name lpcscheddX.fnal.gov, indicate 'X'"
 
-
-for sample in dirList:
-    os.system('eos root://cmseos.fnal.gov/ mkdir -p '+outDir+sample)
-    os.system('mkdir -p '+condorDir+sample)
-    relPath = sample
-
-    rootfiles = EOSlist_root_files(inputDir+sample)
-    tmpcount = 0
-    for i in range(0,len(rootfiles),20):
-        rawname = relPath
-        count += 1
-        tmpcount += 1
-
-        minid = i+1
-        maxid = i+20
-        if maxid > len(rootfiles): maxid = len(rootfiles)
-        print minid, maxid
-
-        dict={'RUNDIR':runDir, 'POST':runDirPost, 'RELPATH':relPath, 'CONDORDIR':condorDir, 'INPUTDIR':inDir, 'FILENAME':rawname, 'PROXY':proxyPath, 'CMSSWBASE':relbase, 'OUTPUTDIR':outDir, 'MIN':minid, 'MAX':maxid, 'ID':tmpcount}
-        jdfName=condorDir+'/%(RELPATH)s/%(FILENAME)s_%(ID)s.job'%dict
-        print jdfName
-        jdf=open(jdfName,'w')
-        jdf.write(
-            """x509userproxy = %(PROXY)s
-universe = vanilla
-Executable = %(RUNDIR)s/makeStep1.sh
-Should_Transfer_Files = YES
-WhenToTransferOutput = ON_EXIT
-Transfer_Input_Files = %(RUNDIR)s/makeStep1.C, %(RUNDIR)s/%(POST)s/step1.cc, %(RUNDIR)s/%(POST)s/step1.h, %(RUNDIR)s/%(POST)s/step1_cc.d, %(RUNDIR)s/%(POST)s/step1_cc.so
-Output = %(FILENAME)s_%(ID)s.out
-Error = %(FILENAME)s_%(ID)s.err
-Log = %(FILENAME)s_%(ID)s.log
-Notification = Never
-Arguments = %(FILENAME)s %(FILENAME)s %(INPUTDIR)s/%(RELPATH)s %(OUTPUTDIR)s/%(RELPATH)s %(MIN)i %(MAX)i
-
-Queue 1"""%dict)
-        jdf.close()
-        os.chdir('%s/%s'%(condorDir,relPath))
-        os.system('condor_submit %(FILENAME)s_%(ID)s.job'%dict)
-        os.system('sleep 0.5')
-        os.chdir('%s'%(runDir))
-        print count, "jobs submitted!!!"
-        print "You can kill jobs by: condor_rm -all -name lpcscheddX.fnal.gov, indicate 'X'"
-
-
-
 for sample in ttbarList:
     rootfiles = EOSlist_root_files(inputDir+sample)
     relPath = sample
@@ -197,6 +153,53 @@ Queue 1"""%dict)
             os.system('sleep 0.5')
             os.chdir('%s'%(runDir))
             print count, "jobs submitted!!!"
+
+
+for sample in dirList:
+    os.system('eos root://cmseos.fnal.gov/ mkdir -p '+outDir+sample)
+    os.system('mkdir -p '+condorDir+sample)
+    relPath = sample
+
+    rootfiles = glob.glob(inputDir+sample+sampleCatTag+"/*/0000/*")
+    print rootfiles
+    tmpcount = 0
+    for i in range(0,len(rootfiles),20):
+        rawname = relPath
+        count += 1
+        tmpcount += 1
+
+        minid = i+1
+        maxid = i+20
+        if maxid > len(rootfiles): maxid = len(rootfiles)
+        print minid, maxid
+
+        dict={'RUNDIR':runDir, 'POST':runDirPost, 'RELPATH':relPath, 'CONDORDIR':condorDir, 'INPUTDIR':inDir, 'FILENAME':rawname, 'PROXY':proxyPath, 'CMSSWBASE':relbase, 'OUTPUTDIR':outDir, 'MIN':minid, 'MAX':maxid, 'ID':tmpcount}
+        jdfName=condorDir+'/%(RELPATH)s/%(FILENAME)s_%(ID)s.job'%dict
+        print jdfName
+        jdf=open(jdfName,'w')
+        jdf.write(
+            """x509userproxy = %(PROXY)s
+universe = vanilla
+Executable = %(RUNDIR)s/makeStep1.sh
+Should_Transfer_Files = YES
+WhenToTransferOutput = ON_EXIT
+Transfer_Input_Files = %(RUNDIR)s/makeStep1.C, %(RUNDIR)s/%(POST)s/step1.cc, %(RUNDIR)s/%(POST)s/step1.h, %(RUNDIR)s/%(POST)s/step1_cc.d, %(RUNDIR)s/%(POST)s/step1_cc.so
+Output = %(FILENAME)s_%(ID)s.out
+Error = %(FILENAME)s_%(ID)s.err
+Log = %(FILENAME)s_%(ID)s.log
+Notification = Never
+Arguments = %(FILENAME)s %(FILENAME)s %(INPUTDIR)s/%(RELPATH)s %(OUTPUTDIR)s/%(RELPATH)s %(MIN)i %(MAX)i
+
+Queue 1"""%dict)
+        jdf.close()
+        os.chdir('%s/%s'%(condorDir,relPath))
+        os.system('condor_submit %(FILENAME)s_%(ID)s.job'%dict)
+        os.system('sleep 0.5')
+        os.chdir('%s'%(runDir))
+        print count, "jobs submitted!!!"
+        print "You can kill jobs by: condor_rm -all -name lpcscheddX.fnal.gov, indicate 'X'"
+
+
 
 print("--- %s minutes ---" % (round(time.time() - start_time, 2)/60))
 
--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #The only few lines you need to modify for your each task
-export jobid=2017/Feb_addNumCounter #will create this as an output folder in /eos/uscms/store/user/lpcljm/yiting/${jobid}/nominal
+export sampleCatTag=singleLep2017
+#export jobid=2017/FWLJMET #will create this as an output folder in /eos/uscmstore/user/lpcljm/yiting/${jobid}/nominal
 
-export ntupleWhere=/store/user/lpcljm/2018/LJMet94X_1lep_013019 #need to point where is the nutple loacted at
-export slimrootWhere=/store/user/lpcljm/yiting/${jobid}  
+export ntupleWhere=/eos/uscms/store/user/lpcljm/FWLJMET102X_1lep2017_052219 #need to point where is the nutple loacted at
+export slimrootWhere=/eos/uscms/store/user/lpcljm/yiting/${sampleCatTag}/FWLJMET  
 
 
 #nothing you need to change
 export ANABASE=${PWD}
-export logbase=${ANABASE}/log/${jobid}
-export rootbase=${ANABASE}/results/${jobid}
+export logbase=${ANABASE}/log/${sampleCatTag}/FWLJMET
+export rootbase=${ANABASE}/results/${sampleCatTag}/FWLJMET
 
